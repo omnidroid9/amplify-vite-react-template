323a213c1c4c4dbefba62b1d54d57ad45f9ed0a7
Geoff Buhn <geoff.buhn@gmail.com>
2024-11-24 09:20:53 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,67 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import React from "react";
+// import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+  console.log("hello consolo 2")
   
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ werid new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}
-          </li>
-        ))}
-      </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <h1>Welcome to Amplify + Vite + React + TypeScript!</h1>
+        <p></p>
       </div>
-    </main>
   );
 }
 
 export default App;
+
+
+
+
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+
+// const client = generateClient<Schema>();
+
+// function App() {
+//   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+//   useEffect(() => {
+//     client.models.Todo.observeQuery().subscribe({
+//       next: (data) => setTodos([...data.items]),
+//     });
+//   }, []);
+
+//   function createTodo() {
+//     client.models.Todo.create({ content: window.prompt("Todo content") });
+//   }
+
+  
+//   function deleteTodo(id: string) {
+//     client.models.Todo.delete({ id })
+//   }
+
+//   return (
+//     <main>
+//       <h1>My todos</h1>
+//       <button onClick={createTodo}>+ werid new</button>
+//       <ul>
+//         {todos.map((todo) => (
+//           <li 
+//           onClick={() => deleteTodo(todo.id)}
+//           key={todo.id}>{todo.content}
+//           </li>
+//         ))}
+//       </ul>
+//       <div>
+//         🥳 App successfully hosted. Try creating a new todo.
+//         <br />
+//         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+//           Review next step of this tutorial.
+//         </a>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
